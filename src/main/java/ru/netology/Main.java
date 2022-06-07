75cb5650e71f2c1a18902ac4882b6c9b65a6318a
Русин Алексей <aleksej-rusin@mail.ru>
2022-06-07 20:11:22 +0300

    ERROR empty JSON file

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -7,13 +7,9 @@ import com.opencsv.CSVWriter;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import org.w3c.dom.NamedNodeMap;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+import org.w3c.dom.*;
 import org.xml.sax.SAXException;
 
-import javax.swing.text.Document;
-import javax.swing.text.Element;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -26,26 +22,25 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
 
         //создаем CSV файл с данными
         String fileNameCSV = "data.csv";
         createCSV(fileNameCSV);
 
-//        String fileNameXML = "data.xml";
-        File fileXML = new File("data.xml");
+        String fileNameXML = "data.xml";
 
         String fileNameCsvJson = "data.json";
         String fileNameXmlJson = "data2.json";
 
-        //парсим CSV и записываем в JSON
+        //CSV  записываем в JSON
         List<Employee> listCSV = parseCSV(columnMapping, fileNameCSV);
         if (listCSV != null) listToJson(listCSV, fileNameCsvJson);
 
-        //парсим XML и записываем в JSON
+        //XML  записываем в JSON
         try {
-            List<Employee> listXML = parseXML(fileXML);
+            List<Employee> listXML = parseXML(fileNameXML);
             listToJson(listXML, fileNameXmlJson);
         } catch (ParserConfigurationException | IOException | SAXException e) {
             e.printStackTrace();
@@ -73,56 +68,55 @@ public class Main {
     }
 
     //чтение XML
-    public static List<Employee> parseXML(File file) throws ParserConfigurationException, IOException, SAXException, ClassCastException {
-        List<Employee> listEmployee = new ArrayList<>();
+    public static List<Employee> parseXML(String fileNameXML) throws ParserConfigurationException, IOException, SAXException, ClassCastException {
 
         DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
         DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
-        Document doc = (Document) documentBuilder.parse(file);
+        Document doc = documentBuilder.parse(new File(fileNameXML));
 
+        Node root = doc.getDocumentElement();
 
-        Node root = (Node) doc.getDefaultRootElement();
-
-        read(root, listEmployee);
-
-
-        return listEmployee;
+        return read(root);
     }
 
-    private static void read(Node node, List<Employee> listEmployee) {
+    private static List<Employee> read(Node node) {
         NodeList nodeList = node.getChildNodes();
+        List<Employee> listEmployee = new ArrayList<>();
 
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node_ = nodeList.item(i);
-            if (Node.ELEMENT_NODE == node_.getNodeType() && "employee".equals(node_.getNodeName())) {
+            if (Node.ELEMENT_NODE == node_.getNodeType()) {
 
-                Employee newEmployee = new Employee();
+                System.out.println(node_.getNodeName());
                 Element element = (Element) node_;
-                NamedNodeMap map = (NamedNodeMap) element.getAttributes();
+                NamedNodeMap map = element.getAttributes();
+                Employee employee = new Employee();
 
                 for (int a = 0; a < map.getLength(); a++) {
                     if (map.item(a).getNodeName().equals("id")) {
-                        newEmployee.id = Long.parseLong(map.item(a).getNodeValue());
+                        employee.setId(Long.parseLong(map.item(a).getNodeValue()));
                     } else if (map.item(a).getNodeName().equals("firstName")) {
-                        newEmployee.firstName = map.item(a).getNodeValue();
+                        employee.setFirstName(map.item(a).getNodeValue());
                     } else if (map.item(a).getNodeName().equals("lastName")) {
-                        newEmployee.lastName = map.item(a).getNodeValue();
+                        employee.setLastName(map.item(a).getNodeValue());
                     } else if (map.item(a).getNodeName().equals("country")) {
-                        newEmployee.country = map.item(a).getNodeValue();
+                        employee.setCountry(map.item(a).getNodeValue());
                     } else if (map.item(a).getNodeName().equals("age")) {
-                        newEmployee.age = Integer.parseInt(map.item(a).getNodeValue());
+                        employee.setAge(Integer.parseInt(map.item(a).getNodeValue()));
                     }
                 }
-                listEmployee.add(newEmployee);
+                if (employee.id != 0) listEmployee.add(employee);
             }
+
         }
+        return listEmployee;
     }
 
     //делаем строку JSON
     public static void listToJson(List<Employee> list, String fileName) {
 
         //создаем файл JSON
-        try (FileWriter file = new FileWriter(fileName, true)) {
+        try (FileWriter file = new FileWriter(fileName)) {
             //получаем строку в формате JSON и записываем
             for (Employee employee : list) {
                 try {
@@ -143,7 +137,7 @@ public class Main {
     public static void createCSV(String fileName) {
 
         String[] columnMapping1 = {"1", "John", "Smith", "USA", "25"};
-        String[] columnMapping2 = {"2", "Inav", "Petrov", "RU", "23"};
+        String[] columnMapping2 = {"2", "Ivan", "Petrov", "RU", "23"};
 
         try (CSVWriter writer = new CSVWriter(new FileWriter(fileName))) {
             writer.writeNext(columnMapping1);
